0e73a91250039cb6fb35d8b63e5222be54e65168
detailyang <detailyang@gmail.com>
2016-03-08 17:36:36 +0800

    misc commit

---
+++ static/src/mixins/EditModal.js
@@ -0,0 +1,31 @@
+import Antd from 'antd'
+
+const handleEditModalOk = function() {
+    this.model.set(this.state.formData)
+    this.setState({confirmLoading: true})
+
+    this.model.save()
+        .always(() => {
+            this.setState({confirmLoading: false})
+        })
+        .done(() => {
+            const msg = this.model.get('id') ? '编辑成功' : '创建成功'
+            Antd.message.success(msg)
+            this.props.onOk()
+        })
+        .fail((msg, resp) => {
+            if (resp.data && resp.data.errors) {
+                this.setState({formErrors: resp.data.errors})
+            }
+            Antd.message.error(msg, 3)
+        })
+}
+
+const handleEditModalCancel = function() {
+    this.props.onCancel()
+}
+
+export default {
+    handleEditModalOk: handleEditModalOk,
+    handleEditModalCancel: handleEditModalCancel
+}
