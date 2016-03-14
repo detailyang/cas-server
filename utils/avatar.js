59e4354114712091a6bb346580f42fb76f464dc5
detailyang <detailyang@gmail.com>
2016-03-14 16:33:03 +0800

    add uglifyjs

--- utils/avatar.js
+++ utils/avatar.js
@@ -1,20 +1,20 @@
 /*
-* @Author: detailyang
-* @Date:   2016-03-10 12:52:31
-* @Last Modified by:   detailyang
-* @Last Modified time: 2016-03-10 13:02:19
-*/
+ * @Author: detailyang
+ * @Date:   2016-03-10 12:52:31
+* @Last modified by:   detailyang
+* @Last modified time: 2016-03-14T15:58:21+08:00
+ */
 
-'use strict';
-var avatar = require('avatar-generator')();
+
+const avatar = require('avatar-generator')();
 
 module.exports = {
-    generate(id, sex, width) {
-        return new Promise((resolve, reject)=> {
-            avatar(id, sex, width).toBuffer(function (err,buffer){
-                if (err) return reject(err);
-                resolve(buffer);
-            });
-        });
-    }
-}
+  generate(id, sex, width) {
+    return new Promise((resolve, reject) => {
+      avatar(id, sex, width).toBuffer((err, buffer) => {
+        if (err) return reject(err);
+        resolve(buffer);
+      });
+    });
+  },
+};
