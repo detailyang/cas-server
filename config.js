8b5be90436fc97038bf340e72b629e261e29675c
detailyang <detailyang@gmail.com>
2016-03-14 15:25:12 +0800

    add production setting

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * @Author: detailyang
  * @Date:   2015-02-19 14:09:05
 * @Last modified by:   detailyang
-* @Last modified time: 2016-03-14T00:28:20+08:00
+* @Last modified time: 2016-03-14T15:24:55+08:00
  */
 const config = module.exports;
 
@@ -65,5 +65,55 @@ if (process.env.NODE_ENV === 'dev') {
 } else if (process.env.NODE_ENV === 'test') {
   console.log('test');
 } else {
-  console.log('prod');
+  config.database = {
+    username: process.env.CAS_DATABASE_USERNAME,
+    password: process.env.CAS_DATABASE_PASSWORD,
+    database: process.env.CAS_DATABASE_DATABASE,
+    host: process.env.CAS_DATABASE_HOST,
+    port: process.env.CAS_DATABASE_PORT,
+    dialect: 'mysql',
+    logging: false,
+  };
+  config.redis = {
+    host: process.env.CAS_REDIS_HOST,
+    port: process.env.CAS_REDIS_PORT,
+    ttl: process.env.CAS_REDIS_TTL || 3600,
+    db: process.env.CAS_REDIS_DB,
+    key: process.env.CAS_REDIS_KEY || 'momtellmewhy',
+  };
+  config.password = {
+    'default': process.env.CAS_PASSWORD_DEFAULT || 'password',
+    'bcryptlength': process.env.CAS_PASSWORD_BCRYPTLENGTH || 12,
+  };
+  config.avatar = {
+    width: 100,
+    cache: 3 * 24 * 60 * 60,
+    maxsize: 512 * 1024 * 1024,
+  };
+  config.notp = {
+    label: process.env.CAS_NOTP_LABEL || 'cas',
+    delta: process.env.CAS_NOTP_DELTA || '-3',
+    salt: process.env.CAS_NOTP_SALT || '$2a$10$jsZ0onecMnHOeKUfRG9AYe',
+  };
+  config.syslog = {
+    tag: process.env.CAS_SYSLOG_TAG || 'cas',
+    facility: process.env.CAS_SYSLOG_FACILITY || 'local6',
+    hostname: process.env.CAS_SYSLOG_HOSTNAME || '192.168.66.204',
+    port: process.env.CAS_SYSLOG_PORT,
+  };
+  config.queue = {
+    name: process.env.CAS_QUEUE_NAME || 'cas',
+    hostname: process.env.CAS_QUEUE_HOSTNAME || '127.0.0.1',
+    port: process.env.CAS_QUEUE_PORT,
+    db: process.env.CAS_QUEUE_DB || 1,
+  };
+  config.cache = {
+    host: process.env.CAS_CACHE_HOST,
+    port: process.env.CAS_CACHE_PORT,
+    ttl: process.env.CAS_CACHE_TTL || 3600,
+    db: process.env.CAS_CACHE_DB || 2,
+  };
+  config.oauth = {
+    ttl: 1 * 60,
+  };
 }
