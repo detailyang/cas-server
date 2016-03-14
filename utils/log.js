956228d0918db1a8b9d9bcf319e4c711c4d9a641
detailyang <detailyang@gmail.com>
2016-03-14 17:48:09 +0800

    use winston

--- utils/log.js
+++ utils/log.js
@@ -2,19 +2,21 @@
 * @Author: detailyang
 * @Date:   2016-02-18 20:38:38
 * @Last modified by:   detailyang
-* @Last modified time: 2016-03-13T19:16:09+08:00
+* @Last modified time: 2016-03-14T17:47:37+08:00
 */
 
 
 import config from '../config';
+import winston from 'winston';
+require('winston-syslog').Syslog;
 
-const SysLogger = require('ain2');
-const logger = new SysLogger({
-  tag: config.syslog.tag,
-  facility: config.syslog.facility,
-  hostname: config.syslog.hostname,
+
+winston.add(winston.transports.Syslog, {
+  host: config.syslog.hostname,
   port: config.syslog.port,
-},
-);
+  facility: config.syslog.facility,
+  app_name: config.syslog.tag,
+});
+winston.remove(winston.transports.Console);
 
-module.exports = logger;
+module.exports = winston;
