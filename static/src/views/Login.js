265e18346675ffdbebcb1063114d340e77db6922
detailyang <detailyang@gmail.com>
2016-04-04 23:23:03 +0800

    add oauth model type field to markup auth type

--- static/src/views/Login.js
+++ static/src/views/Login.js
@@ -3,7 +3,7 @@
 * @Date:   2016-03-14T10:30:11+08:00
 * @Email:  detailyang@gmail.com
 * @Last modified by:   detailyang
-* @Last modified time: 2016-04-04T23:15:58+08:00
+* @Last modified time: 2016-04-04T23:17:46+08:00
 * @License: The MIT License (MIT)
 */
 
