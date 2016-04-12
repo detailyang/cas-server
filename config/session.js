62a85335dd212578a949dfd5fc7371427fc96c3b
detailyang <detailyang@gmail.com>
2016-04-12 18:17:39 +0800

    checkout process.env always string:(

--- config/session.js
+++ config/session.js
@@ -3,7 +3,7 @@
 * @Date:   2016-03-16T22:03:58+08:00
 * @Email:  detailyang@gmail.com
 * @Last modified by:   detailyang
-* @Last modified time: 2016-04-12T17:47:24+08:00
+* @Last modified time: 2016-04-12T18:17:33+08:00
 * @License: The MIT License (MIT)
 */
 
@@ -40,8 +40,8 @@ if (process.env.NODE_ENV === 'dev') {
     db: process.env.CAS_SESSION_DB || 0,
     cookiekey: process.env.CAS_SESSION_COOKIEKEY || 'cas',
     key: process.env.CAS_SESSION_KEY || 'iamyoufather',
-    secure: process.env.CAS_SESSION_SECURE || true,
-    http_only: process.env.CAS_SESSION_HTTPONLY || true,
+    secure: +process.env.CAS_SESSION_SECURE || false,
+    http_only: +process.env.CAS_SESSION_HTTPONLY || false,
     domain: process.env.CAS_SESSION_DOMAIN,
   };
 }
