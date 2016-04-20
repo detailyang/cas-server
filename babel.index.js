59ad647e916ab0679e5e68e97c4a361f43fe5b0d
detailyang <detailyang@gmail.com>
2016-04-20 19:50:47 +0800

    use babelrc to replace hard code

--- babel.index.js
+++ babel.index.js
@@ -1,5 +1,25 @@
-require('babel-core/register')({
-  presets: ['es2015-node5', 'stage-3'],
-});
+/**
+* @Author: BingWu Yang <detailyang>
+* @Date:   2016-03-14T10:29:31+08:00
+* @Email:  detailyang@gmail.com
+* @Last modified by:   detailyang
+* @Last modified time: 2016-04-20T19:47:18+08:00
+* @License: The MIT License (MIT)
+*/
+
+
+const fs = require('fs');
+
+const babelrc = fs.readFileSync('./.babelrc');
+const config;
+
+try {
+  config = JSON.parse(babelrc);
+} catch (err) {
+  console.error('==>     ERROR: Error parsing your .babelrc.');
+  console.error(err);
+}
+
+require('babel-core/register')(config);
 require('babel-polyfill');
 require('./index.js');
