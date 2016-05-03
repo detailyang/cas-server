0d57dee75cd1ed7c780be6dce195b2412f7647bc
detailyang <detailyang@gmail.com>
2016-05-03 11:53:42 +0800

    adjust static dir

--- src/middlewares/serve.js
+++ src/middlewares/serve.js
@@ -8,6 +8,6 @@ import convert from 'koa-convert';
 import path from 'path';
 import staticCache from 'koa-static-cache';
 
-module.exports = convert(staticCache(path.join(__dirname, '../static'), {
+module.exports = convert(staticCache(path.join(__dirname, '../../static'), {
   maxAge: 3 * 24 * 60 * 60,
 }));
