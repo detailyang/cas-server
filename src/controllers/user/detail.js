769d7952ada531c69e6c0b0d8fbfc0c53573ec54
detailyang <detailyang@gmail.com>
2016-06-14 17:40:31 +0800

    remove define but unused

--- src/controllers/user/detail.js
+++ src/controllers/user/detail.js
@@ -2,12 +2,11 @@
  * @Author: detailyang
  * @Date:   2016-02-29 14:32:13
 * @Last modified by:   detailyang
-* @Last modified time: 2016-06-14T17:38:20+08:00
+* @Last modified time: 2016-06-14T17:40:21+08:00
  */
 import fs from 'fs';
 import zxcvbn from 'zxcvbn';
 import sshpk from 'sshpk';
-import crypto from 'crypto';
 import { md5 } from 'utility';
 
 import models from '../../models';
